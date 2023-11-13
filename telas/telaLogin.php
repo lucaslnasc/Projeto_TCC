4e921d82c8b375aefd2c75f7752676cb7e201229
lucaslnasc <lucaslnascimento015@gmail.com>
2023-11-13 17:10:39 -0300

    crianção da tela de login e do css

---
+++ telas/telaLogin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="shortcut icon" href="logologominimini-removebg-preview.png" type="image/x-icon">
+    <link rel="stylesheet" href="cssLogin.css">
+</head>
+<body>
+    <header>
+        <div id="headerCima">
+            <div class="logozada">
+                <img src="LOGOFIMSEM-removebg-preview (1).png" alt="Logo do sistema">
+            </div>
+        </div>
+    </header>
+
+    <form action="" method="post">
+        <div class="container">
+            <h3 class="tituloTest">Fazer Login</h3>
+            <div class="area-login">
+            <label for="">E-mail</label>
+            <input type="text" name="email" class="form-control inputs" aria-describedby="addon-wrapping" required>
+            <label for="">Senha</label>
+            <input type="password" class="form-control inputs" aria-describedby="addon-wrapping" required>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
