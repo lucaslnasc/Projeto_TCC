c3e249fbecf8f3815248ab390882ead4ba6f1c04
lucaslnasc <lucaslnascimento015@gmail.com>
2023-11-13 21:48:24 -0300

    finalização da tela de login

--- telas/telaCadastrar.php
+++ telas/telaCadastrar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="shortcut icon" href="logologominimini-removebg-preview.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logologominimini-removebg-preview.png" type="image/x-icon">
     <link rel="stylesheet" href="cssCadastrar.css">
     <title>TELA CADASTRO</title>
 </head>
@@ -13,7 +13,7 @@
 
       <div id="headerCima">
       <div class="logozada">
-            <a href="http://"><img src="LOGOFIMSEM-removebg-preview (1).png" alt=""></a>
+            <a href="http://"><img src="../img/LOGOFIMSEM-removebg-preview (1).png" alt=""></a>
         </div>
       </div>
 
