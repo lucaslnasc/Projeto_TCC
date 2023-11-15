841fd0c0055de3b32e5dc9625c44e1b24c1b6e76
GabrielSilvadp <gabrielsilvadp04@gmail.com>
2023-11-15 04:44:54 -0300

    new cadastro

---
+++ telas/newCad.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="../img/logologominimini-removebg-preview.png" type="image/x-icon">
+    <link rel="stylesheet" href="cssCadastrar.css">
+    <title>TELA CADASTRO</title>
+</head>
+<body>
+<header>
+
+      <div id="headerCima">
+      <div class="logozada">
+            <a href="http://"><img src="../img/LOGOFIMSEM-removebg-preview (1).png" alt=""></a>
+        </div>
+      </div>
+
+      <div id="headerBaixo">
+        <div id="rodape"></div>
+      </div>
+
+      <label for="">
+
+      </label>
+    </header>
+     
+<form action="" method="post">
+    <div class="fundinho">
+      <label for="">Informações Pessoais</label>
+      <hr>
+      <div class="input-group">
+      <input type="text" id="nome" class="input">
+      <label for="nome" class="label">Nome</label>
+      </div>
+</div>
+
+    </form>
+
+  </div>
+  </div>
+</div>
+</body>
+</html>
