3931cb00491f1409f21b4846b29dd450b7c1ed24
GabrielSilvadp <gabrielsilvadp04@gmail.com>
2023-11-19 01:25:55 -0300

    tela cadastro cartão

--- telas/telaPrincipal.php
+++ telas/telaPrincipal.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./CSS/cssPrincipal.css">
+    <link rel="shortcut icon" href="../img/logologominimini-removebg-preview.png" type="image/x-icon">
     <title>Vaga Certa</title>
 </head>
 
