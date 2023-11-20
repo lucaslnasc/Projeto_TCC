bd9550c654beee09e2f2682006f0ced67552ca62
lucaslnasc <lucaslnascimento015@gmail.com>
2023-11-20 15:18:12 -0300

    inner join dentro do banco de dados e esqueleto pronto da tela de favoritos

---
+++ telas/telaFavorito.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaga Certa</title>
+    <link rel="shortcut icon" href="../img/logologominimini-removebg-preview.png" type="image/x-icon">
+    <link rel="stylesheet" href="./CSS/cssFavoritos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <body class="padrao0">
+        <header>
+            <div id="headerCima">
+                <div class="logozada">
+                    <a href="http://"><img src="../img/LOGOFIMSEM-removebg-preview (1).png" alt="Logo do sistema"></a>
+                </div>
+            </div>
+            <div id="headerMeio">
+                <div class="novoMenu">
+                    <hr class="linha">
+                    <ul class="menuLateral">
+                        <li><a href=""><img src="../img/Subtract.png" alt="Icone de inicio">Inicio</a></li>
+                    </ul>
+                    <hr class="linha">
+                    <ul class="menuLateral">
+                        <li><a href=""><img src="../img/User_fill.png" alt="Icone de Perfil">Conta</a></li>
+                    </ul>
+                    <hr class="linha">
+                    <ul class="menuLateral">
+                        <li><a href=""><img src="../img/Favorite.png" alt="Icone de Favorito">Favorito</a></li>
+                    </ul>
+                    <hr class="linha">
+                    <ul class="menuLateral">
+                        <li><a href="telaCartoes.php"><img src="../img/Credit_card.png"
+                                    alt="Icone de Cartão">Cartões</a></li>
+                    </ul>
+                    <hr class="linha">
+
+                    <footer class="inferiorLateral">
+                        <a href="telaLogin.php"><img src="../img/Import.png" alt="Sair icone">Sair</a>
+                    </footer>
+                </div>
+            </div>
+            <div id="headerBaixo">
+                <div id="rodape"></div>
+            </div>
+        </header>
+    </body>
+
+</html>
