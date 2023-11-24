88e682c3472bf21fbd82a5e313ef5e6026a080f7
GabrielSilvadp <gabrielsilvadp04@gmail.com>
2023-11-24 13:56:06 -0300

    criação da telaConfirmaAgend - Gabriel

---
+++ telas/telaConfirmaAgend.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaga Certa</title>
+    <link rel="shortcut icon" href="../img/logologominimini-removebg-preview.svg" type="image/x-icon">
+    <link rel="stylesheet" href="./CSS/cssConfirmaAgend.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body class="padrao0">
+        <div class="logozada">
+            <a href="http://"><img src="../img/LOGOFIMSEM-removebg-preview (1) (1).svg" alt=""></a>
+        </div>
+
+        <div class="fundo">
+            <label id="txtCima">seu agendamento foi realizado com sucesso!</label>
+            <div class="linha"></div>
+            <span class="spanNome">Nome Completo</span><br>
+            <input class="inputNome" type="text" required="required" placeholder="Nome completo">
+            <span class="spanVaga">Vaga</span>
+            <input class="inputVaga" type="text" required="required" placeholder="Vaga">
+            <span class="spanTel">Telefone</span>
+            <input class="inputTel" type="text" required="required" placeholder="Telefone">
+            <span class="spanData">Data</span>
+            <input type="text" class="inputData" required="required" placeholder="Data">
+            <span class="spanPV">placa do veículo</span>
+            <input type="text" class="inputPV" required="required" placeholder="placa do veículo">
+            <span class="spanHorario">Horário</span>
+            <input type="text" class="inputHorario" required="required" placeholder="Horário">
+            <button class="btSair">sair</button>
+            <button class="btQR">gerar qr code</button>
+        </div>
+
+</body>
+</html>
