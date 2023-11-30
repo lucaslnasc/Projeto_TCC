e2eb862caea1e2235c2196c422c1b4cc74dd420c
lucaslnasc <lucaslnascimento015@gmail.com>
2023-11-30 15:55:26 -0300

    telaAddCartao ajustada e algumas manutenções - Lucas

--- bd/validarLogin.php
+++ bd/validarLogin.php
@@ -1,44 +1,47 @@
 <?php
- 
-    include("conexao.php");
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+include("conexao.php");
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
 
-    if (empty($email) || empty($senha)){
+if (empty($email) || empty($senha)) {
 
-        echo "<script>alert('Campos obrigatórios vazios')</script>";
-    }else{
-  
-        try{
-      
-            
-            $query = $dbh->prepare("SELECT id_usuario, email, senha FROM usuario WHERE email = :email AND senha = :senha");
-            $query->execute(array(
+    echo "<script>alert('Campos obrigatórios vazios')</script>";
+} else {
+
+    try {
+
+
+        $query = $dbh->prepare("SELECT id_usuario, email, senha FROM usuario WHERE email = :email AND senha = :senha");
+        $query->execute(
+            array(
                 ':email' => $email,
                 ':senha' => $senha
-                )
-            );
-        
-            $resultado = $query->fetch();
-            
-        
-            if(empty($resultado)){
-                echo "<script>alert('Usuário e/ou senha invalidos')</script>";
-            }
-           
-           
-            else{
-                if(!isset($_SESSION)){
-                    session_start();
-                } 
-                $_SESSION['id_usuario'] = $resultado['id_usuario'];
-                $_SESSION['email'] = $resultado['email'];
-                header('Location: ../telas/telaPrincipal.php');
-            }
+            )
+        );
 
-        }catch(Exception $e){
-            echo $e;
+        $resultado = $query->fetch();
+
+
+        if (empty($resultado)) {
+            echo "<script>
+            alert('Usuário e/ou senha invalidos');
+            window.location.href='../telas/telaLogin.php';
+            </script>";
         }
+        
+        else {
+            if (!isset($_SESSION)) {
+                session_start();
+            }
+            $_SESSION['id_usuario'] = $resultado['id_usuario'];
+            $_SESSION['email'] = $resultado['email'];
+            header('Location: ../telas/telaPrincipal.php');
+        }
+
+    } catch (Exception $e) {
+        echo $e;
     }
+}
